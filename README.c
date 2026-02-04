d9ecae0ab6ffb5de107579c709372eca5631728c
dhruv10-ja <1262252800@mitwpu.edu.in>
2026-02-04 15:08:24 +0530

    Implement Armstrong number checker in README.c
    Add a C program to check if a number is an Armstrong number.

--- README.c
+++ README.c
@@ -1,1 +1,29 @@
 # assignment-10
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, remainder, result = 0;
+ ("Enter a three-digit integer: ");
+    scanf("%d", &num);
+    
+    originalNum = num;   
+    printf
+
+    while (originalNum != 0) {
+       
+        remainder = originalNum % 10;
+        
+       
+        result += remainder * remainder * remainder;
+        
+       
+        originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
